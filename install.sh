6c0b59b4ded16758a1e9dd8e8bcb384a1ebef0d2
itaicaspi-intel <itai.caspi@intel.com>
2018-05-22 11:01:58 +0300

    constraining gym installation to version 0.9.4

--- install.sh
+++ install.sh
@@ -109,7 +109,7 @@ IN_VIRTUAL_ENV=`python3 -c 'import sys; print("%i" % hasattr(sys, "real_prefix")
 
 # basic installations
 sudo -E apt-get install python3-pip cmake zlib1g-dev python3-tk python-opencv -y
-pip3 install --upgrade pip
+#pip3 install --upgrade pip
 
 # if we are not in a virtual environment, we will create one with the appropriate python version and then activate it
 # if we are already in a virtual environment,
@@ -165,7 +165,7 @@ if [ ${INSTALL_GYM} -eq 1 ]; then
     echo "Installing Gym support"
     sudo -E apt-get install libav-tools libsdl2-dev swig cmake -y
     pip3 install box2d # for bipedal walker etc.
-    pip3 install gym
+    pip3 install gym[all]==0.9.4
 fi
 
 # NGraph and Neon
