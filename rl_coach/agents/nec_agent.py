5f31b3ef2d6f5a07d14bf4c7cb6863b416529ecf
Gal Leibovich <gal.leibovich@intel.com>
2019-03-26 12:47:16 +0200

    fix for NEC agent

--- rl_coach/agents/nec_agent.py
+++ rl_coach/agents/nec_agent.py
@@ -162,7 +162,7 @@ class NECAgent(ValueOptimizationAgent):
             embedding = self.networks['main'].online_network.predict(
                 self.prepare_batch_for_inference(self.curr_state, 'main'),
                 outputs=self.networks['main'].online_network.state_embedding)
-            self.current_episode_state_embeddings.append(embedding)
+            self.current_episode_state_embeddings.append(embedding.squeeze())
 
         return super().act()
 
