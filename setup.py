98af7f151234a8046d92c7155b698c9c65d42458
Shadi Endrawis <shadi.endrawis@intel.com>
2019-04-03 09:28:25 +0300

    update version

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ extras['all'] = all_deps
 
 setup(
     name='rl-coach' if not slim_package else 'rl-coach-slim',
-    version='0.11.1',
+    version='0.11.2',
     description='Reinforcement Learning Coach enables easy experimentation with state of the art Reinforcement Learning algorithms.',
     url='https://github.com/NervanaSystems/coach',
     author='Intel AI Lab',
