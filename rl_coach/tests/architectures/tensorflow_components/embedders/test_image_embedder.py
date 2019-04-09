b6f871845ec844e22cea9a87f4e423131f9fb2ba
James Casbon <jcasbon@tower-research.com>
2019-04-09 14:09:56 +0100

    Revert "tests/heads"
    This reverts commit 60390a8307269be8ee99625a011229ccdf3f5ced.

--- rl_coach/tests/architectures/tensorflow_components/embedders/test_image_embedder.py
+++ rl_coach/tests/architectures/tensorflow_components/embedders/test_image_embedder.py
@@ -25,10 +25,8 @@ def test_embedder(reset):
     with pytest.raises(ValueError):
         embedder = ImageEmbedder(np.array([10, 100, 100, 100]), name="test")
 
-
-    is_training = tf.Variable(False, trainable=False, collections=[tf.GraphKeys.LOCAL_VARIABLES])
     # creating a simple image embedder
-    embedder = ImageEmbedder(np.array([100, 100, 10]), name="test", is_training=False)
+    embedder = ImageEmbedder(np.array([100, 100, 10]), name="test")
 
     # make sure the ops where not created yet
     assert len(tf.get_default_graph().get_operations()) == 0
@@ -57,9 +55,7 @@ def test_embedder(reset):
 @pytest.mark.unit_test
 def test_complex_embedder(reset):
     # creating a deep vector embedder
-    is_training = tf.Variable(False, trainable=False, collections=[tf.GraphKeys.LOCAL_VARIABLES])
-    embedder = ImageEmbedder(np.array([100, 100, 10]), name="test", scheme=EmbedderScheme.Deep, 
-        is_training=is_training)
+    embedder = ImageEmbedder(np.array([100, 100, 10]), name="test", scheme=EmbedderScheme.Deep)
 
     # call the embedder
     embedder()
@@ -75,9 +71,8 @@ def test_complex_embedder(reset):
 @pytest.mark.unit_test
 def test_activation_function(reset):
     # creating a deep image embedder with relu
-    is_training = tf.Variable(False, trainable=False, collections=[tf.GraphKeys.LOCAL_VARIABLES])
     embedder = ImageEmbedder(np.array([100, 100, 10]), name="relu", scheme=EmbedderScheme.Deep,
-                             activation_function=tf.nn.relu, is_training=is_training)
+                             activation_function=tf.nn.relu)
 
     # call the embedder
     embedder()
