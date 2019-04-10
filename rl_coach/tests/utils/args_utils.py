adacc4d7e6a623cf1e90fbc582748cf4976c50f5
anabwan <ayoob.nabwani@intel.com>
2019-04-10 03:52:27 -0400

    tests: minor fix for functional tests

--- rl_coach/tests/utils/args_utils.py
+++ rl_coach/tests/utils/args_utils.py
@@ -395,6 +395,9 @@ def validate_arg_result(flag, p_valid_params, clres=None, process=None,
                 results.append(last_step[-1])
                 time.sleep(1)
 
+        # get the first value after heat-up (should be bigger than the cp value
+        results.append(csv["Total steps"].values[-1])
+
         assert int(results[-1]) >= Def.Consts.num_hs, \
             Def.Consts.ASSERT_MSG.format("bigger than " +
                                          str(Def.Consts.num_hs), results[-1])
