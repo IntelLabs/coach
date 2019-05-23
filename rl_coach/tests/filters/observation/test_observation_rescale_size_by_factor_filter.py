227b7784d580595af5163278d173d017051d4213
Gal Leibovich <gal.leibovich@intel.com>
2019-05-23 10:15:21 +0300

    moving to skimage.transform.resize

--- rl_coach/tests/filters/observation/test_observation_rescale_size_by_factor_filter.py
+++ rl_coach/tests/filters/observation/test_observation_rescale_size_by_factor_filter.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import pytest
 import numpy as np
 
-from rl_coach.filters.observation.observation_rescale_size_by_factor_filter import ObservationRescaleSizeByFactorFilter, RescaleInterpolationType
+from rl_coach.filters.observation.observation_rescale_size_by_factor_filter import ObservationRescaleSizeByFactorFilter
 from rl_coach.spaces import ObservationSpace
 from rl_coach.core_types import EnvResponse
 from rl_coach.filters.filter import InputFilter
@@ -17,7 +17,7 @@ def test_filter():
     env_response = EnvResponse(next_state={'observation': np.ones([20, 30, 3])}, reward=0, game_over=False)
     rescale_filter = InputFilter()
     rescale_filter.add_observation_filter('observation', 'rescale',
-                                          ObservationRescaleSizeByFactorFilter(0.5, RescaleInterpolationType.BILINEAR))
+                                          ObservationRescaleSizeByFactorFilter(0.5))
 
     result = rescale_filter.filter(env_response)[0]
     unfiltered_observation = env_response.next_state['observation']
@@ -33,7 +33,7 @@ def test_filter():
     env_response = EnvResponse(next_state={'observation': np.ones([20, 30])}, reward=0, game_over=False)
     rescale_filter = InputFilter()
     rescale_filter.add_observation_filter('observation', 'rescale',
-                                          ObservationRescaleSizeByFactorFilter(2, RescaleInterpolationType.BILINEAR))
+                                          ObservationRescaleSizeByFactorFilter(2))
     result = rescale_filter.filter(env_response)[0]
     filtered_observation = result.next_state['observation']
 
@@ -47,7 +47,7 @@ def test_get_filtered_observation_space():
     # error on wrong number of channels
     rescale_filter = InputFilter()
     rescale_filter.add_observation_filter('observation', 'rescale',
-                                          ObservationRescaleSizeByFactorFilter(0.5, RescaleInterpolationType.BILINEAR))
+                                          ObservationRescaleSizeByFactorFilter(0.5))
     observation_space = ObservationSpace(np.array([10, 20, 5]))
     with pytest.raises(ValueError):
         filtered_observation_space = rescale_filter.get_filtered_observation_space('observation', observation_space)
@@ -64,3 +64,6 @@ def test_get_filtered_observation_space():
 
     # make sure the original observation space is unchanged
     assert np.all(observation_space.shape == np.array([10, 20, 3]))
+
+if __name__ == '__main__':
+    test_filter()
