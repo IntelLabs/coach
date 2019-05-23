0a17ca6421dc22daae4a140303aea6e1f5287dd7
Gal Leibovich <gal.leibovich@intel.com>
2019-05-23 16:12:38 +0300

    update number of episodes for the test (as it reflects epochs, and not episodes)

--- rl_coach/presets/CartPole_DQN_BatchRL_BCQ.py
+++ rl_coach/presets/CartPole_DQN_BatchRL_BCQ.py
@@ -134,7 +134,7 @@ env_params = GymVectorEnvironment(level='CartPole-v0')
 preset_validation_params = PresetValidationParameters()
 preset_validation_params.test = True
 preset_validation_params.min_reward_threshold = 150
-preset_validation_params.max_episodes_to_achieve_reward = 2000
+preset_validation_params.max_episodes_to_achieve_reward = 50
 
 graph_manager = BatchRLGraphManager(agent_params=agent_params,
                                     experience_generating_agent_params=experience_generating_agent_params,
