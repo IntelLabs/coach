0a49ea4e5a989c6ae37853720d7c94cc052a24d6
Dan Elbaz <danelbaz@danelbaz-mac02.iil.intel.com>
2019-07-16 10:34:55 +0300

    Added __init_ files to tf2_components sub folders

---
+++ rl_coach/architectures/tf2_components/heads/__init__.py
@@ -0,0 +1,43 @@
+from .q_head import QHead
+from .categorical_q_head import CategoricalQHead
+from .ddpg_actor_head import DDPGActor
+from .dnd_q_head import DNDQHead
+from .dueling_q_head import DuelingQHead
+from .measurements_prediction_head import MeasurementsPredictionHead
+from .naf_head import NAFHead
+from .policy_head import PolicyHead
+from .ppo_head import PPOHead
+from .ppo_v_head import PPOVHead
+from .quantile_regression_q_head import QuantileRegressionQHead
+from .rainbow_q_head import RainbowQHead
+from .v_head import VHead
+from .acer_policy_head import ACERPolicyHead
+from .sac_head import SACPolicyHead
+from .sac_q_head import SACQHead
+from .classification_head import ClassificationHead
+from .cil_head import RegressionHead
+from .td3_v_head import TD3VHead
+from .ddpg_v_head import DDPGVHead
+
+__all__ = [
+    'CategoricalQHead',
+    'DDPGActor',
+    'DNDQHead',
+    'DuelingQHead',
+    'MeasurementsPredictionHead',
+    'NAFHead',
+    'PolicyHead',
+    'PPOHead',
+    'PPOVHead',
+    'QHead',
+    'QuantileRegressionQHead',
+    'RainbowQHead',
+    'VHead',
+    'ACERPolicyHead',
+    'SACPolicyHead',
+    'SACQHead',
+    'ClassificationHead',
+    'RegressionHead',
+    'TD3VHead'
+    'DDPGVHead'
+]
