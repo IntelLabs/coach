7381c83f1db7d54fc0899e1b9c471798767b2846
Gal Leibovich <gal.leibovich@intel.com>
2019-09-05 17:16:18 +0300

    test updates

--- rl_coach/presets/Mujoco_Wolpertinger.py
+++ rl_coach/presets/Mujoco_Wolpertinger.py
@@ -16,10 +16,9 @@ from rl_coach.agents.wolpertinger_agent import WolpertingerAgentParameters
 ####################
 schedule_params = ScheduleParameters()
 schedule_params.improve_steps = EnvironmentSteps(2000000)
-schedule_params.steps_between_evaluation_periods = EnvironmentEpisodes(100)
+schedule_params.steps_between_evaluation_periods = EnvironmentEpisodes(20)
 schedule_params.evaluation_steps = EnvironmentEpisodes(1)
-schedule_params.heatup_steps = EnvironmentSteps(10000)
-# schedule_params.heatup_steps = EnvironmentSteps(100)
+schedule_params.heatup_steps = EnvironmentSteps(3000)
 
 #########
 # Agent #
@@ -33,7 +32,6 @@ agent_params.network_wrappers['critic'].input_embedders_parameters['action'].sch
 agent_params.output_filter = \
     OutputFilter(
         action_filters=OrderedDict([
-            # ('discretization', BoxDiscretization(num_bins_per_dimension=int(1e4)))
             ('discretization', BoxDiscretization(num_bins_per_dimension=int(1e6)))
         ]),
         is_a_reference_filter=False
@@ -49,10 +47,10 @@ env_params = GymVectorEnvironment(level=SingleLevelSelection(mujoco_v2))
 ########
 preset_validation_params = PresetValidationParameters()
 preset_validation_params.test = True
-preset_validation_params.min_reward_threshold = 100
-preset_validation_params.max_episodes_to_achieve_reward = 3000
-preset_validation_params.reward_test_level = 'cartpole'
-preset_validation_params.trace_test_levels = ['cartpole']
+preset_validation_params.min_reward_threshold = 500
+preset_validation_params.max_episodes_to_achieve_reward = 1000
+preset_validation_params.reward_test_level = 'inverted_pendulum'
+preset_validation_params.trace_test_levels = ['inverted_pendulum']
 
 graph_manager = BasicRLGraphManager(agent_params=agent_params, env_params=env_params,
                                     schedule_params=schedule_params, vis_params=VisualizationParameters(),
