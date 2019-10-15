Fixed Bug in value loss calculation. Reaching reward of 1000, not stable

rl_coach/architectures/tensorflow_components/dnn_model.py
@@ -343,7 +343,7 @@ class ModelWrapper(object):
 
         inputs = list(map(lambda x: tf.keras.layers.Input(shape=x), self._input_shapes))
         outputs = model(inputs)
-        self.model = keras.Model(inputs=inputs, outputs=outputs)
+        self.model = keras.Model(name=network_name, inputs=inputs, outputs=outputs)
         # Instantiate the sub moduls
         #self.model(self.dummy_model_inputs)
 

rl_coach/architectures/tensorflow_components/heads/ppo_head.py
@@ -73,8 +73,8 @@ class ContinuousPPOHead(keras.layers.Layer):
         """
         policy_means = self.policy_means_layer(inputs)
         #log_stds = self.policy_log_std_layer(inputs.numpy())
-        log_stds = self.policy_log_std_layer(tf.stop_gradient(inputs))
-        #log_stds = self.policy_log_std_layer(inputs)
+        #log_stds = self.policy_log_std_layer(tf.stop_gradient(inputs))
+        log_stds = self.policy_log_std_layer(inputs)
         policy_stds = tf.exp(log_stds)
 
 

rl_coach/architectures/tensorflow_components/losses/v_loss.py
@@ -48,8 +48,9 @@ class VLoss(HeadLoss):
         )
 
     def loss_forward(self, value_prediction, target):
-
-        loss = tf.reduce_mean(self.loss_fn(value_prediction, target))
+        target = tf.reshape(target, shape=value_prediction.shape)
+        loss = self.loss_fn(value_prediction, target)
+        #loss = tf.reduce_mean(self.loss_fn(value_prediction, target))
         # TODO: preferable to return a tensor containing one loss per instance, rather than returning the mean loss.
         #  This way, Keras can apply class weights or sample weights when requested.
         return [(loss, LOSS_OUT_TYPE_LOSS)]