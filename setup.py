dced13e15ac59a0d4fe600b5c5c51cf8abbc0a10
Gal Novik <gal.novik@intel.com>
2020-06-29 11:36:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -66,14 +66,7 @@ using_GPU = out != ''
 
 if not using_GPU:
     if not slim_package:
-        # For linux wth no GPU, we install the Intel optimized version of TensorFlow
-        if sys.platform == "linux" or sys.platform == "linux2":
-            # CI: limiting version to 1.13.1 due to
-            # https://github.com/tensorflow/tensorflow/issues/29617
-            # (reproduced with intel-tensorflow 1.14.0 but not with 1.13.1)
-            install_requires.append('intel-tensorflow==1.13.1')
-        else:
-            install_requires.append('tensorflow>=1.9.0,<=1.14.0')
+        install_requires.append('tensorflow>=1.9.0,<=1.14.0')
     extras['mxnet'] = ['mxnet-mkl>=1.3.0']
 else:
     if not slim_package:
