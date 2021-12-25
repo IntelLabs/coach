628cf04248d073ae09c5886b4522b004679fa703
Théo <theo.vincent@eleves.enpc.fr>
2021-12-25 22:25:59 +0000

    Small changes

--- fetch_checkpoints_from_local.sh
+++ fetch_checkpoints_from_local.sh
@@ -4,4 +4,4 @@ IP_ADRESS=$1
 SERVER_END_PATH=$2
 CLIENT_END_PATH=$3
 
-scp -r ubuntu@$1:/home/theovincent/MVA/ObjectRecognition/LearningToAct/coach/$2 /home/developer/LearningToAct/coach/$3
+scp -r theo.vincent@$1:/home/theovincent/MVA/ObjectRecognition/LearningToAct/coach/$2 /home/developer/LearningToAct/coach/$3
